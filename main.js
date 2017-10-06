c26ffa9fe582d9d4647b5fc1b55a1755cd4f3e68
potatowow <u0224004@mis.nkfust.edu.tw>
2017-10-06 15:19:03 +0800

    20171006_update
    修改navbar深度
    新增Top按鈕

--- main.js
+++ main.js
@@ -3,4 +3,14 @@ $(document).ready(function() {
 	$(this).toggleClass('active');
 	$('#overlay').toggleClass('open');
 	});
-})
+});
+
+/*scrollTop*/
+$(function() {
+	$('#gotop').click(function(){
+		$('html, body').animate({
+			scrollTop:0
+		},1000);
+		return false;
+	});
+});
